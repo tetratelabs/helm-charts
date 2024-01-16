4313aed3c42ca27bc41b8327b3919d35eef12a7c
Mohammad Anwari <mdamt@mdamt.net>
2024-01-16 17:50:04 +0700

    Use PR strategy

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
-VERSION=$(echo $1 | sed -e 'sXrefs/heads/update-istio-XX')
+VERSION=$(echo $1 | sed -e 'sXupdate-istio-XX')
 URL=$2
+CURRENT_BRANCH=$1
+
 
 if [ -d $VERSION ]; then
     echo "Version $VERSION must be specified"
@@ -20,13 +22,14 @@ trap "cd $HERE && rm -rf $TMPDIR" EXIT
 
 ITEMS=$(ls -1 $HERE/charts/istio/$VERSION)
 
-git checkout istio-$VERSION
+git checkout $CURRENT_BRANCH
 git fetch origin 
-git switch -t gh-pages;true
-git switch gh-pages;true
-cp -f index.yaml $TMPDIR
-git checkout istio-$VERSION
+git switch gh-pages
+if [ -f index.yaml ]; then
+    cp -f index.yaml $TMPDIR
+fi
 
+git checkout $CURRENT_BRANCH
 git branch
 
 cd $TMPDIR
@@ -40,7 +43,11 @@ for ITEM in $ITEMS; do
     LAST_ITEM=/$ITEM-$ISTIO_VERSION.tgz/
     echo "Processing $ITEM $ISTIO_VERSION $CHART_URL"
     helm package $HERE/charts/istio/$VERSION/$ITEM
-    helm repo index  . --merge $TMPDIR/index.yaml --url $CHART_URL 
+    if [ -f $TMPDIR/index.yaml ]; then
+        helm repo index  . --merge $TMPDIR/index.yaml --url $CHART_URL 
+    else
+        helm repo index  . --url $CHART_URL 
+    fi
 done
 
 mkdir $HERE/build
@@ -54,7 +61,7 @@ cp -f $TMPDIR/index.yaml $HERE/pages
 cd $HERE
 
 if [ -n "GITHUB_OUTPUT" ]; then
-    echo "INDEX=$HERE/pages/index.yaml" >> "$GITHUB_OUTPUT"
+    echo "INDEX=$HERE/pages" >> "$GITHUB_OUTPUT"
     echo "VERSION=$VERSION" >> "$GITHUB_OUTPUT"
     echo "BUILDDIR=$HERE/build" >> "$GITHUB_OUTPUT"
 fi
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,6 @@
 #/bin/sh
 
-VERSION=$(echo $1 | sed -e 'sXrefs/tags/istio-XX')
-URL=$2
+VERSION=$(echo $1 | sed -e 'sXupdate-istio-XX')
 
 if [ -d $VERSION ]; then
     echo "Version $VERSION must be specified"
@@ -15,50 +14,11 @@ if [ ! -d $HERE/charts/istio/$VERSION ]; then
     exit 1
 fi
 
-TMPDIR=$(mktemp -d)
-trap "cd $HERE && rm -rf $TMPDIR" EXIT
-
-ITEMS=$(ls -1 $HERE/charts/istio/$VERSION)
-
-git checkout istio-$VERSION
-git fetch origin 
-git switch -t gh-pages;true
-git switch gh-pages;true
-cp -f index.yaml $TMPDIR
-git checkout istio-$VERSION
-
-git branch
-
-cd $TMPDIR
-
-for ITEM in $ITEMS; do
-    if [ ! -f $HERE/charts/istio/$VERSION/$ITEM/Chart.yaml ]; then
-        continue
-    fi
-    ISTIO_VERSION=$(cat $HERE/charts/istio/$VERSION/$ITEM/Chart.yaml| grep version: | awk '{print $2}')
-    CHART_URL=$URL/istio-$VERSION/$ITEM-$ISTIO_VERSION.tgz
-    LAST_ITEM=/$ITEM-$ISTIO_VERSION.tgz/
-    echo "Processing $ITEM $ISTIO_VERSION $CHART_URL"
-    helm package $HERE/charts/istio/$VERSION/$ITEM
-    helm repo index  . --merge $TMPDIR/index.yaml --url $CHART_URL 
-done
-
-mkdir $HERE/build
-cp -f $TMPDIR/*.tgz $HERE/build
-
-sed -i "sX${LAST_ITEM}X/Xg" $TMPDIR/index.yaml
-
-mkdir $HERE/pages
-cp -f $TMPDIR/index.yaml $HERE/pages
-
-cd $HERE
-git switch gh-pages
-cp pages/index.yaml index.yaml
-git add index.yaml
-rm -rf charts scripts pages
+# $HERE/build contains the tarballs
+# $HERE/pages contains the index.yaml
 
 if [ -n "GITHUB_OUTPUT" ]; then
-    echo "INDEX=$TMPDIR/index.yaml" >> "$GITHUB_OUTPUT"
+    echo "INDEX=$HERE/index.yaml" >> "$GITHUB_OUTPUT"
     echo "VERSION=$VERSION" >> "$GITHUB_OUTPUT"
     echo "BUILDDIR=$HERE/build" >> "$GITHUB_OUTPUT"
 fi
